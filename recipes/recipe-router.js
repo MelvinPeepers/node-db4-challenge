01deedd351addf6c79c7b1d6bf136a490933d8f1
MelvinPeepers <peepersm@gmail.com>
2019-08-31 11:41:59 -0400

    setup recipe-router

--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -1,7 +1,52 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js")
+const db = require("../recipes/recipe-router.js")
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Recipes.getRecipes()
+    .then(recipe => {
+      res.json(recipe);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Failed to get recipes' });
+    });
+  });
+  
+
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    Recipess.getShoppingList(id)
+    .then(recipe => {
+      if (scheme) {
+        res.json(recipe);
+      } else {
+        res.status(404).json({ message: 'Could not find recipe with given id.' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Failed to get recipe' });
+    });
+  });
+  
+  
+  
+  router.get('/:id/instructions', (req, res) => {
+    const { id } = req.params;
+  
+    Recipes.getInstructions(id)
+    .then(instruction => {
+      if (instruction.length) {
+        res.json(instruction);
+      } else {
+        res.status(404).json({ message: 'Could not find instructions for given recipe' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Failed to get steps' });
+    });
+  });
+ 
 module.exports = router;
