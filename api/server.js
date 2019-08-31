dd525f96c1e851a754ba694ab10cc9244a114624
MelvinPeepers <peepersm@gmail.com>
2019-08-31 10:04:01 -0400

    correction with file name and path in server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const recipesRouter = require("../recipe/recipe-router.js");
+const recipesRouter = require("../recipes/recipe-router.js");
 
 const server = express();
 
