ea576fe708a5ed968a8f392ad91bd9bb7f05a91b
MelvinPeepers <peepersm@gmail.com>
2019-09-01 13:27:37 -0400

    created migration

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -5,3 +5,22 @@ getRecipes,
 getShoppingList,
 getInstructions
 };
+
+function getRecipes() {
+    return db('recipes');
+}
+
+// GET ingredients and quantities for a given recipe
+function getShoppingList(id) {
+    return db('recipes')
+    .where({ id })
+    .first()
+}
+
+// GET a list of step by step instructions for preparing a recipe 
+function getInstructions(scheme_id) {
+    return db('steps as st')
+    .join('schemes as s', 's.id', 'st.scheme_id')
+    .select('st.scheme_id', 's.scheme_name', 'st.step_number', 'st.instructions')
+    .where({ scheme_id })
+}
